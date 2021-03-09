a7d6cfe28b9e1726297bd360cb467c39dbfe2b0e
Ángel Cano Y <nikeindex@gmail.com>
2021-03-09 11:58:39 +0100

    Update bookmarklet.js

--- bookmarklet.js
+++ bookmarklet.js
@@ -1,19 +1,20 @@
-if ( window.location = "https://www.dondominio.com/buscar/" || window.location = "https://www.mrdomain.com/buscar/") {
-    var disponible = document.querySelectorAll("div.domainsearch-available");
-    var resultados = [];
-    if (Object.keys(disponible).length > 0) {
-        Object.entries(disponible).forEach(([key, value]) => {
-            resultados.push([value.childNodes[0].textContent, value.childNodes[3].textContent]);
+if ( window.location == "https://www.dondominio.com/buscar/" || window.location == "https://www.mrdomain.com/buscar/") {
+    var available = document.querySelectorAll("div.domainsearch-available");
+    var results = [];
+    if (Object.keys(available).length > 0) {
+        Object.entries(available).forEach(([key, value]) => {
+            results.push([value.childNodes[0].textContent, value.childNodes[3].textContent]);
         });
-        crearTabla(resultados);
+        createTable(results);
     }
-    function crearTabla(dominios) {
-        var tabla = '<table><thead><th>dominio</th><th>precio(s)</th></thead><tbody>';
-        for (var i = 0; i < dominios.length; i++) {
-            tabla += '<tr><td>' + dominios[i][0] + '</td><td>' + dominios[i][1] + '</td></tr>';
+    function createTable(domains) {
+        var myTable = '<table><thead><th>Domain</th><th>Price(s)</th></thead><tbody>';
+        for (var i = 0; i < domains.length; i++) {
+            myTable += '<tr><td>' + domains[i][0] + '</td><td>' + domains[i][1] + '</td></tr>';
         };
-        tabla += '</table>';
-        var nuevaVentana = window.open("")
-        nuevaVentana.document.write(tabla);
+        myTable += '</table>';
+        var myWindow = window.open("");
+        myWindow.document.write(myTable);
+
     }
 }
